b3b5c9a2665568a9a8dcdc497c6507c53a3a2dfe
Debra Gordon <debra.m.gordon@gmail.com>
2017-02-01 15:55:51 -0800

    started program file and expression files

--- SimpleCalculator/Program.cs
+++ SimpleCalculator/Program.cs
@@ -10,6 +10,31 @@ namespace SimpleCalculator
     {
         static void Main(string[] args)
         {
+            
+            ////// foundational elements
+            // initialize Parser HERE
+            // initialize Evaluator HERE
+
+            while (true)
+            {
+                // ask the user for a basic mathematical equation
+                Console.WriteLine("Please type a basic expression you would like me to find the answer to.");
+                Console.WriteLine("Include one of the following operators: + - * /.");
+                string userInput = Console.ReadLine();
+
+                //evaluate input
+                try
+                {
+                    // send user input to parser function
+                    string calculatorAnswer = ""; // the result of the evaluate functions is stored in this variable
+                    Console.WriteLine(calculatorAnswer);
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine($"Sorry, { userInput} is not a valid request.");
+                    Console.WriteLine("Try again, something more simple like: 2 + 1. Take it easy on me");
+                }
+            }
         }
     }
 }
