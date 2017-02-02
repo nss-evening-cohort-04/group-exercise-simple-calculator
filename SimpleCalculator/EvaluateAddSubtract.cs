93f7a20535a537f59b28e57433d8a1c61d6a201e
GAylabea <gbea@rocketmail.com>
2017-02-01 18:55:59 -0600

    create add and subtract evaluate and test file

---
+++ SimpleCalculator/EvaluateAddSubtract.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleCalculator
+{
+    public class Evaluate 
+    {
+        public int Add(int First, int Second)
+        {
+            return First + Second;
+        }
+        public int Subtract(int First, int Second)
+        {
+            return First - Second;
+        }
+    }
+
+
+}
