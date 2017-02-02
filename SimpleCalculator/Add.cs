45f076654e55ea4d0c5b9141fd96e146e99fece2
OwenHerndon <owen.w.herndon@gmail.com>
2017-02-01 16:20:55 -0800

    added testing files

--- SimpleCalculator/Add.cs
+++ SimpleCalculator/Add.cs
@@ -3,15 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using System.Text.RegularExpressions;
 
 namespace SimpleCalculator
 {
-    class Program
+    class Add
     {
-        static void Main(string[] args)
-        {
-            
-        }
     }
 }
--- SimpleCalculator/Integer.cs
+++ SimpleCalculator/Integer.cs
@@ -3,15 +3,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using System.Text.RegularExpressions;
 
 namespace SimpleCalculator
 {
-    class Program
+    public class Integer
     {
-        static void Main(string[] args)
-        {
-            
-        }
+
     }
 }
