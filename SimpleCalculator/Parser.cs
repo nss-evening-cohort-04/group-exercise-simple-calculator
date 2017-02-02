cff418d31144de6fd431534292755e3abb7e2327
Dustin Reed <dmreedy1@gmail.com>
2017-02-01 16:55:22 -0800

    working on parser tests cont

---
+++ SimpleCalculator/Parser.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace SimpleCalculator
+{
+    public class Parser
+    {
+       
+        public Expression ParseInput(string input)
+        {
+
+            char[] operators = new char[] { '+', '-', '*', '%', '/' };
+            try
+            {
+                string[] splitStr = input.Trim().Split(operators);
+                string firstTerm;
+                string secondTerm;
+                char mathOperator;
+                if (splitStr.Length > 1)
+                {
+                    firstTerm = splitStr[0].Trim();
+                    mathOperator = input[splitStr[0].Length];
+                    secondTerm = splitStr[1].Trim();
+                }
+                else
+                {
+
+                };
+                    
+                return new Expression(firstTerm, secondTerm, mathOperator);
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+
+        }
+
+
+    }
+}
