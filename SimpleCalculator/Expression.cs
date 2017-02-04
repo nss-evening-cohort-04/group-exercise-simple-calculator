2cf74247139a70baef2f75e08af9ecf9490330a4
Taylor Shuff <tks2n@mtmail.mtsu.edu>
2017-02-04 11:13:34 -0600

    added expression class and implementation in program.cs

--- SimpleCalculator/Expression.cs
+++ SimpleCalculator/Expression.cs
@@ -9,10 +9,42 @@ namespace SimpleCalculator
 {
     public class Expression
     {
-        //string pattern = ;
-       public string input = "2+2";
-
-        Match m = Regex.Match(@"2+2", @" ^ (?<number1>\d+)\s*(?<operator>[+\/\-%*]+)\s*(?<number2>\d+)\s*$");
         
-    }
+        public string Operator { get; set; }
+        public string leftValue { get; set; }
+        public string rightValue { get; set; }
+        //string pattern = ;
+       //public string input = "2+2";
+
+        public Expression()
+        {
+            Operator = "";
+            leftValue = "";
+            rightValue = "";
+        }
+
+        public static string[] getMathExpression(string arg)
+        {
+            Console.WriteLine("Expression Class.");
+
+            Regex r = new Regex(@"^(?<number1>\w+)\s*(?<operator>[+\/\-%*]+)\s*(?<number2>\w+)\s*$", RegexOptions.IgnoreCase);
+
+            //Match m = r.Match("2+2");
+            Match m = r.Match(arg);
+
+
+            if (m.Success)
+            {
+                var temporaryInstance = new Expression();
+                temporaryInstance.leftValue = (string)m.Groups["number1"].Value;
+                temporaryInstance.Operator = m.Groups["operator"].Value;
+                temporaryInstance.rightValue = (string)m.Groups["number2"].Value;
+                return new string[] {temporaryInstance.leftValue, temporaryInstance.Operator, temporaryInstance.rightValue };
+            }else
+            {
+                return new string[] { };
+            }
+
+        }
+}
 }
--- SimpleCalculator/Program.cs
+++ SimpleCalculator/Program.cs
@@ -11,8 +11,16 @@ namespace SimpleCalculator
     {
         static void Main(string[] args)
         {
+
             int counter = 0;
             Console.WriteLine($"[{counter}]");
+            
+            var command = Console.ReadLine();
+
+            var responseTest = Expression.getMathExpression(command);
+            Console.WriteLine(responseTest[2]);
+
+
             Console.ReadLine();
 
         }
