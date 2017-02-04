76038170077343891b526995770c93c3720f6ea1
Debra Gordon <debra.m.gordon@gmail.com>
2017-02-04 11:10:37 -0800

    we hit a wall with stack ... very close

---
+++ SimpleCalculator/Stack.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleCalculator
+{
+    class Stack
+    {
+        public string LastQuery { get; set; }
+
+        public int LastAnswer { get; set; }
+
+        public string setLastQuery(string userQuestion)
+        {
+            LastQuery = userQuestion;
+            return LastQuery;
+        }
+
+        public int setLastAnswer(int calculatorAnswer)
+        {
+            LastAnswer = calculatorAnswer;
+            return LastAnswer;
+        }
+
+
+    }
+}
