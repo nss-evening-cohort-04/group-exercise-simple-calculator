d75d79377c69230550472fc304249e9d2b128a6c
Debra Gordon <debra.m.gordon@gmail.com>
2017-02-06 19:09:45 -0800

    added parser tests

--- SimpleCalculator.Tests/ParserTests.cs
+++ SimpleCalculator.Tests/ParserTests.cs
@@ -8,21 +8,79 @@ namespace SimpleCalculator.Tests
     public class ParserTests
     {
         [TestMethod]
-        public void TestMethod1()
+        public void TestFindAddition()
         {
             Parser parser = new Parser();
-            parser.ParseInput("1 + 1");
-            //Assert.IsInstanceOfType(actualValue, typeof(Expression));
-
+            parser.ParseInput("2 + 2");
+            Assert.AreEqual(2, parser.FirstTerm);
+            Assert.AreEqual(2, parser.SecondTerm);
+            Assert.AreEqual('+', parser.Operator);
         }
 
         [TestMethod]
-        public void Test2()
+        public void TestFindSubtraction()
         {
-            /*Parser parser = new Parser();
-            string expectedValue = "last";
-            var actualValue = parser.ParseInput("last");
-            Assert.AreEqual(actualValue, expectedValue);*/
+            Parser parser = new Parser();
+            parser.ParseInput("1212-2");
+            Assert.AreEqual(1212, parser.FirstTerm);
+            Assert.AreEqual(2, parser.SecondTerm);
+            Assert.AreEqual('-', parser.Operator);
+        }
+
+        [TestMethod]
+        public void TestFindMultiply()
+        {
+            Parser parser = new Parser();
+            parser.ParseInput("2 * 211");
+            Assert.AreEqual(2, parser.FirstTerm);
+            Assert.AreEqual(211, parser.SecondTerm);
+            Assert.AreEqual('*', parser.Operator);
+        }
+
+        [TestMethod]
+        public void TestFindDivide()
+        {
+            Parser parser = new Parser();
+            parser.ParseInput("2/12");
+            Assert.AreEqual(2, parser.FirstTerm);
+            Assert.AreEqual(12, parser.SecondTerm);
+            Assert.AreEqual('/', parser.Operator);
+        }
+
+        [TestMethod]
+        public void TestFindModulo()
+        {
+            Parser parser = new Parser();
+            parser.ParseInput("23 % 2");
+            Assert.AreEqual(23, parser.FirstTerm);
+            Assert.AreEqual(2, parser.SecondTerm);
+            Assert.AreEqual('/', parser.Operator);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void TestBadAddition()
+        {
+            Parser parser = new Parser();
+            parser.ParseInput("+2");
+        }
+
+        [TestMethod]
+        public void TestAssigningConstant()
+        {
+            Parser parser = new Parser();
+            parser.ParseInput("x=1");
+            Assert.AreEqual('x', parser.FirstTerm);
+            Assert.AreEqual(1, parser.SecondTerm);
+            Assert.AreEqual('=', parser.Operator);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void TestBadConstantAssignment()
+        {
+            Parser parser = new Parser();
+            parser.ParseInput("1=x");
         }
     }
 }
