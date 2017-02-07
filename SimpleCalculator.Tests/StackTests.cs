0a018f66d250931e1c9edaaca588672f5e630515
Dustin Reed <dmreedy1@gmail.com>
2017-02-06 18:39:34 -0800

    fixed stack tests

--- SimpleCalculator.Tests/StackTests.cs
+++ SimpleCalculator.Tests/StackTests.cs
@@ -9,23 +9,23 @@ namespace SimpleCalculator.Tests
         [TestMethod]
         public void CanIMakeStack()
         {
-            Stack myStack = new Stack;
+            Stack myStack = new Stack();
             Assert.IsNotNull(myStack);
         }
         [TestMethod]
         public void CanISetLastQuery()
         {
             //Assert
-            Stack myStack = new Stack;
-            string lastq = ("2 + 2");
-            Assert.AreEqual(myStack.lastq, ("2 + 2"));
+            Stack myStack = new Stack();
+            myStack.LastQuery = ("2 + 2");
+            Assert.AreEqual(myStack.LastQuery, ("2 + 2"));
         }
         [TestMethod]
         public void CanISetLastAnswer()
         {
-            Stack myStack = new Stack;
-            string lastanswer = "4";
-            Assert.AreEqual(myStack.lastq, "4");
+            Stack myStack = new Stack();
+            myStack.LastAnswer = 4;
+            Assert.AreEqual(myStack.LastAnswer, 4);
         }
     }
 }
